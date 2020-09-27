6ac20a054f3bb6537d42f39a0dbedd2c477df3dd
Taylor Wofford <taylor.wofford@gmail.com>
2020-09-27 15:49:44 -0400

    express routing

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 
 const mongoose = require("mongoose");
 const express = require("express");
-
-
 const app = express();
 const db = require("./config/keys").mongoURI;
+const bodyParser = require("body-parser");
 
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
 
 mongoose
   .connect(db, { useNewUrlParser: true })
@@ -14,10 +15,12 @@ mongoose
 
 
 app.get("/", (req, res) => res.send("Hello World"));
-
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const port = process.env.PORT || 5000;
-
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
